9497f8efed181c3adb2126e01e0a738698416d34
vrmcardoso <vrmcardoso@gmail.com>
2022-11-16 16:10:16 +0000

    Rails yelp mvp done

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates_associated :reviews
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+end
